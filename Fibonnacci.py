1893a34c44bbe7f1f4b305d567ebc50e2c014fb0
Sadn0x <80762342+Sadn0x@users.noreply.github.com>
2021-03-17 11:28:08 -0300

    Create Fibonnacci.py

---
+++ Fibonnacci.py
@@ -0,0 +1,19 @@
+print('-=' * 30)
+print('                   SEQUÊNCIA DE FIBONACCI')
+print('-=' * 30)
+
+termos = int(input('Digite quantos termos você quer visualizar: '))
+t1 = 0
+t2 = 1
+
+print(f'{t1} -> {t2}', end='')
+cont = 3
+
+while cont <= termos:
+    t3 = t1 + t2
+    print(f' -> {t3}', end ='')
+    t1 = t2
+    t2 = t3
+    cont += 1
+    
+print('Fim!')
