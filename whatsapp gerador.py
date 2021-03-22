8a3d81cadebf24259207891352f5823d0e66db9c
Sadn0x <80762342+Sadn0x@users.noreply.github.com>
2021-03-22 14:21:12 -0300

    Update whatsapp gerador.py

--- whatsapp gerador.py
+++ whatsapp gerador.py
@@ -1,6 +1,21 @@
-pais = int(input('Digite o código do país com apenas dois dígitos (exemplo, Brasil é 55): '))
-ddd = int(input('Digite o ddd do estado com apenas dois dígitos (exemplo, Ceará é 85): '))
-telefone = int(input('Digite o número do telefone, apenas dígitos de 0 - 9: '))
+pais = str(input('Digite o código do país com apenas dois dígitos (exemplo, Brasil é 55): '))
+while pais.isdigit() == False:
+    print('Gentileza, digite um código válido')
+    pais = str(input('Digite o código do país: '))
+
+ddd = str(input('Digite o ddd do estado com apenas dois dígitos (exemplo, Ceará é 85): '))
+while ddd.isdigit() == False:
+    print('Gentileza, digite um ddd válido')
+    ddd = str(input('Digite o código do ddd: '))
+telefone = str(input('Digite o número do telefone, apenas dígitos de 0 - 9: '))
+while telefone.isdigit() == False:
+    print('Gentileza, digite um telefone válido')
+    telefone = str(input('Digite o número do telefone: '))
+
+pais = int(pais)
+ddd = int(ddd)
+telefone = int(telefone)
+
 pais *= 100000000000
 ddd *= 1000000000
 if telefone < 900000000:
